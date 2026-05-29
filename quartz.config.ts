6d4d09c8ed17f0edcd57a1b5c83cd1eca6b8c8a9
mostafahemmat <mostafahemmat7@gmail.com>
2026-05-29 06:41:06 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -419,8 +419,7 @@ const colorPalettes = {
 // Select your desired palette here
 // Available palettes: original, coolBlues, warmEarth, forestGreen, royalPurple, oceanicTeal, monochrome, vintageCream, devDark, oceanVibes, halloween, neon, sunset, persianAzure, sakuraDreams, cyberpunkCity, autumnGrove
 // type PaletteName = keyof typeof colorPalettes; // This line might cause issues with older TS
-const currentPaletteName: keyof typeof colorPalettes = "persianAzure"
-
+const currentPaletteName: keyof typeof colorPalettes = "forestGreen"
 const selectedPalette = colorPalettes[currentPaletteName]
 
 /**
