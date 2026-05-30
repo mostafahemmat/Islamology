ad449b8909f51c7ef890b57e26da4684fbf54639
mostafahemmat <mostafahemmat7@gmail.com>
2026-05-30 20:26:49 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -82,7 +82,7 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.Favicon(),
-      Plugin.Manifest(), // <--- این خط برای فعال‌سازی PWA اضافه شده است
+      Plugin.ManifestPage(), // <--- نام اصلاح شده
       Plugin.NotFoundPage(),
       Plugin.CustomOgImages(),
     ],
