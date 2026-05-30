7a4874be1efe37388171f9d8bacbf8fabd5772ad
mostafahemmat <mostafahemmat7@gmail.com>
2026-05-30 20:29:18 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -82,7 +82,6 @@ const config: QuartzConfig = {
       Plugin.Assets(),
       Plugin.Static(),
       Plugin.Favicon(),
-      Plugin.ManifestPage(), // <--- نام اصلاح شده
       Plugin.NotFoundPage(),
       Plugin.CustomOgImages(),
     ],
